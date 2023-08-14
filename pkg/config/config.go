327c51a7d31d69b6bb4e33532098b7c9dfbeb18d
mohamoud aljadan <mahmoudjadan@gmail.com>
2023-08-14 17:56:22 -0300

    refactor: refactor parser use config loader

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,8 +1,41 @@
 package config
 
-type ColumnNames struct {
-	Name   []string `json:"name"`
-	Salary []string `json:"salary"`
-	Email  []string `json:"email"`
-	ID     []string `json:"id"`
+import (
+	"employee-csv-parser/pkg/utils"
+	"encoding/json"
+	"github.com/pkg/errors"
+	"os"
+)
+
+type Loader struct {
+	configPath string
+	config     map[string][]string
+}
+
+func NewConfigLoader(configPath string) *Loader {
+	return &Loader{
+		configPath: configPath,
+	}
+}
+
+func (c Loader) LoadConfig() (map[string][]string, error) {
+	configFile, err := os.Open(c.configPath)
+	if err != nil {
+		return nil, errors.Wrapf(err, "opening config file")
+	}
+	defer configFile.Close()
+
+	decoder := json.NewDecoder(configFile)
+	err = decoder.Decode(&c.config)
+	if err != nil {
+		return nil, errors.Wrapf(err, "error decoding config file [%v]", c.configPath)
+	}
+
+	c.normalizeConfigMap()
+	return c.config, nil
+}
+func (c Loader) normalizeConfigMap() {
+	for k, v := range c.config {
+		c.config[k] = utils.ToLowerTrimSlice(v)
+	}
 }
